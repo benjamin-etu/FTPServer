76d51a69a07e5c9d4340d08d44cecf1114fd24c1
benjamin-etu <benjamin.treels.etu@univ-lille.fr>
2023-01-12 10:32:14 +0100

    refactoring création de méthodes

--- src/FTPServer.java
+++ src/FTPServer.java
@@ -1,59 +1,79 @@
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class FTPServer{
 
     private int port;
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private BufferedReader inFromClient;
+    private DataOutputStream outToClient;
 
-    public FTPServer(int port){
+    public FTPServer(int port) throws IOException{
         this.port = port;
+        this.serverSocket = new ServerSocket(this.port);
     }
 
-    public void run() throws IOException{
-        ServerSocket s = new ServerSocket(this.port);
+    /*
+     * Run Server
+     */
+    public void run(){
         System.out.println("FTP Server listening");
+        try{
+            this.receiveConnections();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
-        // Boucle infinie pour accepter les connexions clientes
+    /*
+     * Receive clients connections 
+     */
+    private void receiveConnections() throws IOException{
         while (true) {
-
-            // Attente d'une connexion cliente
-            Socket clientSocket = s.accept();
+            this.clientSocket = serverSocket.accept();
             System.out.println("Connexion cliente reçue");
 
             // Création des flux d'entrée et sortie
-            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(clientSocket.getOutputStream());
+            this.inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            this.outToClient = new DataOutputStream(clientSocket.getOutputStream());
 
             // Envoi de la réponse au client
             outToClient.writeBytes("220 Server ready\n");
-            
-            /* Lecture des commandes */
-            while (true){
-                String command = inFromClient.readLine();
-                System.out.println(command);
 
-                if (command.startsWith("USER")){
-                    outToClient.writeBytes("331 User name ok, need password\n");
-                }
-                else if (command.startsWith("PASS")){
-                    outToClient.writeBytes("230 User logged in\n");
-                }
-                else if (command.startsWith("QUIT")){
-                    outToClient.writeBytes("Server closes all connections\n"); 
-                    clientSocket.close();
-                    break;
-                }else if (command.startsWith("SYST")){
-                    outToClient.writeBytes("500\n");
-                }else if(command.startsWith("FEAT")){
-                    outToClient.writeBytes("211\n");
-                }
+            this.readCommands();
+        }
+    }
+
+    /*
+     * Read ftp commands and use the correct behavior 
+     */
+    private void readCommands() throws IOException{
+        while (true){
+            String command = inFromClient.readLine();
+            System.out.println(command);
+
+            if (command.startsWith("USER")){
+                outToClient.writeBytes("331 User name ok, need password\n");
+            }
+            else if (command.startsWith("PASS")){
+                outToClient.writeBytes("230 User logged in\n");
+            }
+            else if (command.startsWith("QUIT")){
+                outToClient.writeBytes("Server closes all connections\n"); 
+                clientSocket.close();
+            }
+            else if (command.startsWith("SYST")){
+                outToClient.writeBytes("500\n");
+            }
+            else if(command.startsWith("FEAT")){
+                outToClient.writeBytes("211\n");
             }
         }
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args){
         int port = 4242;
-        FTPServer server = new FTPServer(port);
         try{
+            FTPServer server = new FTPServer(port);
             server.run();
         }catch(IOException e){
             e.printStackTrace();
